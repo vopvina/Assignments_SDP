6ee7fa003843c5f5992803cbbdb331108995b509
vopvina <161079157+vopvina@users.noreply.github.com>
2024-10-13 19:15:31 +0500

    Update Main.java

--- Assignment1and2/src/Main.java
+++ Assignment1and2/src/Main.java
@@ -1,3 +1,44 @@
+//Assignment1 Main.java
+/*import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Singleton
+        CoffeeShop coffeeShop = CoffeeShop.getInstance();
+
+        // Abstract Factory
+        CoffeeIngredientFactory latteFactory = new LatteIngredientFactory(); // Фабрика для ингредиентов латте
+        CoffeeIngredientFactory cappuccinoFactory = new CappuccinoIngredientFactory(); // Фабрика для ингредиентов капучино
+
+        // ввод для заказа кофе
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите тип кофе (IceLatte или Cappuccino): ");
+        String type = scanner.nextLine();
+
+        // Factory Method
+        Coffee coffee = coffeeShop.orderCoffee(type, type.equalsIgnoreCase("IceLatte") ? latteFactory : cappuccinoFactory);
+        System.out.println("Ваш заказ: " + coffee);
+
+        // Prototype
+        Coffee clonedOrder = coffee.clone();
+        System.out.println("Повторный заказ: " + clonedOrder);
+
+        // Builder
+        Coffee customCoffee = new CoffeeBuilder()
+                .setType("Custom Coffee") //
+                .addMilk("Миндальное молоко")
+                .addSyrup("Шоколадный сироп")
+                .build();
+        System.out.println("Ваш кастомный кофе: " + customCoffee);
+
+        // Singleton для получения текущей скидки
+        System.out.println("Скидка на следующий заказ: " + coffeeShop.getDiscount() * 100 + "%");
+    }
+}*/
+
+
+
+//Assignment2 Main.java
 import Assignment2.Facade.DocumentFacade;
 // import Assignment2.Composite.DocumentGroup;
 // import Assignment2.Flyweight.DocumentFactory;
